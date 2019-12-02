9e43b12de6a7d7ad55da8d28c5aa23213a563506
Rabbithols <roberturturica@gmail.com>
2019-12-02 12:24:08 +0100

    refactor: inserito le interfacce nuove

--- src/app/core/models/interfaces.ts
+++ src/app/core/models/interfaces.ts
@@ -1,37 +1,61 @@
-export interface Lingue {
-  Id: string;
-  Descrizione: string;
+export interface Domanda {
+  domanda: Domanda1;
+  errore: string;
+  operazione: number;
 }
-
-export interface Comuni {
-  table?: (Comune)[] | null;
+export interface Domanda1 {
+  id: string;
+  idDomanda: string;
+  versione: number;
+  stato: number;
+  istanzaJSON: string;
+  dataInvio: string;
+  dataModifica: string;
+  anagCandidato: AnagCandidato;
+  titoliStudioPosseduti?: (TitoliStudioPossedutiEntity)[] | null;
+  lingua: LstRiserveEntityOrLstTitoliPreferenzialiEntityOrLingua;
+  lstRiserve?: (LstRiserveEntityOrLstTitoliPreferenzialiEntityOrLingua)[] | null;
+  lstTitoliPreferenziali?: (LstRiserveEntityOrLstTitoliPreferenzialiEntityOrLingua)[] | null;
+  invaliditaCivile: InvaliditaCivile;
 }
-export interface Comune {
-  codISTAT: number;
-  codProvincia: string;
-  comune: string;
-  cap: string;
-  codNazionale: string;
-  codCatastale: string;
-  flagVariazione: number;
+export interface AnagCandidato {
+  codiceFiscale: string;
+  cognome: string;
+  nome: string;
+  dataNascita: string;
+  comuneNascita: ComuneNascitaOrLuogoIstituto;
+  residenza: string;
+  telefono: string;
+  email: string;
 }
-
-export interface Province {
-  table?: (Provincia)[] | null;
+export interface ComuneNascitaOrLuogoIstituto {
+  codice: string;
+  nome: string;
+  codiceProvincia: string;
 }
-export interface Provincia {
-  codProvincia: string;
+export interface TitoliStudioPossedutiEntity {
+  tipologia: TipologiaOrTitoloOrIndirizzo;
+  titolo: TipologiaOrTitoloOrIndirizzo;
+  indirizzo: TipologiaOrTitoloOrIndirizzo;
+  dataConseguimento: string;
+  istituto: string;
+  luogoIstituto: ComuneNascitaOrLuogoIstituto;
+  durataAnni: string;
+}
+export interface TipologiaOrTitoloOrIndirizzo {
+  id: string;
+  desc: string;
+}
+export interface LstRiserveEntityOrLstTitoliPreferenzialiEntityOrLingua {
+  id: number;
+  descrizione: string;
+}
+export interface InvaliditaCivile {
+  percentuale: number;
+  dataCertificazione: string;
+  enteCertificatore: string;
   provincia: string;
-  codRegione: number;
-  regione: string;
-}
-
-export interface Titoli {
-  Id: number;
-  Descrizione: string;
-}
-
-export interface Riserve {
-  Id: number;
-  Descrizione: string;
+  ausili: boolean;
+  tempiAggiuntivi: boolean;
+  esenteProvaPreselettiva: boolean;
 }
