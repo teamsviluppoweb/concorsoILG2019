126a59c60f58e7492f25b42caed6ae010bc2ab36
Rabbithols <roberturturica@gmail.com>
2019-12-12 10:22:11 +0100

    feat: aggiunto provincia e comune a categorie protette

--- src/app/modules/application-form/components/edit/step-categorie-protette/step-categorie-protette.component.ts
+++ src/app/modules/application-form/components/edit/step-categorie-protette/step-categorie-protette.component.ts
@@ -1,8 +1,12 @@
-import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {FormGroup, Validators} from '@angular/forms';
-import {MatStepper} from '@angular/material';
+import {MatSelect, MatStepper} from '@angular/material';
 import {CustomValidators} from '../../../../../shared/validators/customValidators';
 import {DomandaService} from '../../../../../core/services/domanda.service';
+import {Observable, ReplaySubject, Subject} from 'rxjs';
+import {Comune, Provincia} from '../../../../../core/models/rest/rest-interface';
+import {concatMap, filter, take, takeUntil} from 'rxjs/operators';
+import {RestService} from '../../../../../core/services/rest.service';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -10,16 +14,36 @@ import {DomandaService} from '../../../../../core/services/domanda.service';
   templateUrl: './step-categorie-protette.component.html',
   styleUrls: ['./step-categorie-protette.component.scss']
 })
-export class StepCategorieProtetteComponent implements OnInit {
+export class StepCategorieProtetteComponent implements OnInit, OnDestroy {
 
   @Input() parent: FormGroup;
   maxDateDataCertificazione = new Date(Date.now());
   @ViewChild('stepper', { static: false }) private myStepper: MatStepper;
 
-  constructor(private domandaService: DomandaService) {
+  @ViewChild('provinceSelect', { static: true }) provinceSelect: MatSelect;
+  public filtroProvince: ReplaySubject<Provincia[]> = new ReplaySubject<Provincia[]>(1);
+  listaProvince: Provincia[];
+
+  @ViewChild('comuneSelect', { static: true }) comuneSelect: MatSelect;
+  public filtroComuni: ReplaySubject<Comune[]> = new ReplaySubject<Comune[]>(1);
+  listaComuni: Comune[];
+
+
+  private onDetroy = new Subject<void>();
+
+  constructor(private domandaService: DomandaService, private rest: RestService) {
   }
 
   ngOnInit() {
+
+    this.rest.getProvince().subscribe(
+      (data: Provincia[]) => {
+        this.listaProvince = data;
+        this.filtroProvince.next(this.listaProvince.slice());
+        this.setInitialProvinceValue(this.filtroProvince);
+      }
+    );
+
     if (this.domandaService.domandaobj.domanda.stato === 1) {
       if (this.domandaService.domandaobj.domanda.invaliditaCivile.percentuale) {
         this.appartenenza.patchValue('SI');
@@ -38,6 +62,35 @@ export class StepCategorieProtetteComponent implements OnInit {
       }
     }
 
+    this.comune.valueChanges.subscribe( (data) => {
+    });
+
+    this.provincia.valueChanges
+      .pipe(
+        // Mi assicuro che il valore nel form sia valido
+        filter(() => this.provincia.valid),
+        concatMap((data: Provincia) => this.rest.getComuni(data.codice))
+      )
+      .subscribe((data: Comune[]) => {
+        this.listaComuni = data;
+        this.filtroComuni.next(this.listaComuni.slice());
+        console.log(this.listaComuni);
+        this.setInitialComuneValue(this.filtroComuni);
+      });
+
+    // Analizza i cambiamenti del testo nel campo di ricerca del dropdown search dei comuni
+    this.comuniDropdown.valueChanges
+      .pipe(takeUntil(this.onDetroy))
+      .subscribe(() => {
+        this.filtraRicerca(this.listaComuni, this.comuniDropdown, this.filtroComuni);
+      });
+
+    // Analizza i cambiamenti del testo nel campo di ricerca del dropdown search delle province
+    this.provinceDropdown.valueChanges
+      .pipe(takeUntil(this.onDetroy))
+      .subscribe(() => {
+        this.filtraRicerca(this.listaProvince, this.provinceDropdown, this.filtroProvince);
+      });
 
     this.onChanges();
   }
@@ -118,6 +171,57 @@ export class StepCategorieProtetteComponent implements OnInit {
 
   }
 
+
+  private setInitialProvinceValue(data: Observable<Provincia[]> ) {
+    data
+      .pipe(take(1), takeUntil(this.onDetroy))
+      .subscribe(() => {
+        // setting the compareWith property to a comparison function
+        // triggers initializing the selection according to the initial value of
+        // the form control (i.e. _initializeSelection())
+        // this needs to be done after the filteredBanks are loaded initially
+        // and after the mat-option elements are available
+        this.provinceSelect.compareWith = (a: string, b: string) => a && b && a === b;
+      });
+  }
+
+  private setInitialComuneValue(data: Observable<Comune[]>) {
+    data
+      .pipe(take(1), takeUntil(this.onDetroy))
+      .subscribe(() => {
+        // setting the compareWith property to a comparison function
+        // triggers initializing the selection according to the initial value of
+        // the form control (i.e. _initializeSelection())
+        // this needs to be done after the filteredBanks are loaded initially
+        // and after the mat-option elements are available
+        this.comuneSelect.compareWith = (a: string, b: string) => a && b && a === b;
+      });
+  }
+
+  ngOnDestroy() {
+    this.onDetroy.next();
+    this.onDetroy.complete();
+  }
+
+
+  private filtraRicerca(data: (Provincia[] | Comune[]), form, filters) {
+    if (!data) {
+      return;
+    }
+    // ottiene la keyword di ricerca
+    let search = form.value;
+    if (!search) {
+      filters.next(data.slice());
+      return;
+    } else {
+      search = search.toLowerCase();
+    }
+    // Filtra le province
+    filters.next(
+      data.filter(nm => nm.nome.toLocaleLowerCase().indexOf(search) > -1)
+    );
+  }
+
   get appartenenza() {
     return this.parent.get('formCategorieProtette.appartenenza');
   }
@@ -145,6 +249,26 @@ export class StepCategorieProtetteComponent implements OnInit {
   get esenzioneProvaSelettiva() {
     return this.parent.get('formCategorieProtette.esenzioneProvaSelettiva');
   }
+
+
+  // DROPDOWN
+
+  get comune() {
+    return this.parent.get('formCategorieProtette.comune');
+  }
+
+  get comuniDropdown() {
+    return this.parent.get('formCategorieProtette.comuniDropdown');
+  }
+
+  get provincia() {
+    return this.parent.get('formCategorieProtette.provincia');
+  }
+
+  get provinceDropdown() {
+    return this.parent.get('formCategorieProtette.provinceDropdown');
+  }
+
 
 }
 
