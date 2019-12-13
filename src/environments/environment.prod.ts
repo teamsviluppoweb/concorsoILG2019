b3f1a0a6f79a19fde8f1ff61d2c34c74859ad08a
Rabbithols <roberturturica@gmail.com>
2019-12-13 15:46:32 +0100

    chore: cambiato l'environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,26 @@
+
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
 export const environment = {
-  production: true,
-  enableTracing: false,
-  localIp: '192.168.1.152',
-  hostredirect: '',
+  production: false,
+  enableTracing: true,
+
+  localIp: 'pc39548.dipvvf.it',
+  hostredirect: 'https://pc39548.dipvvf.it:4200/',
   endpoint: {
-    titoli: 'http://webpc.dipvvf.it:5001/TitoliPreferenziali/Tutti',
-    riserve: 'http://webpc.dipvvf.it:5001/riserve/iac',
-    lingue: 'http://webpc.dipvvf.it:5001/lingue',
-    province: 'http://webpc.dipvvf.it:5000/api/province',
-    comuni: 'http://webpc.dipvvf.it:5000/api/comuni/prov/',
-  },
+    domanda: 'http://webpc.dipvvf.it:8020/GetDomanda',
+    salvaDomanda: 'http://webpc.dipvvf.it:8020/SalvaDomanda',
+    titoli: 'http://webpc.dipvvf.it:8020/TitoliPreferenziali',
+    riserve: 'http://webpc.dipvvf.it:8020/Riserve',
+    lingue: 'http://webpc.dipvvf.it:8020/Lingue',
+    province: 'http://webpc.dipvvf.it:8020/Province',
+    comuni: 'http://webpc.dipvvf.it:8020/Province/',
+    tipologia: 'http://webpc.dipvvf.it:8020//TitoliStudio/Tipologie',
+    titolo: 'http://webpc.dipvvf.it:8020//TitoliStudio/titoli/',
+    indirizzi: 'http://webpc.dipvvf.it:8020/TitoliStudio/indirizzi/',
+    infoConcorso: 'http://webpc.dipvvf.it:8020/info/',
+  }
 };
