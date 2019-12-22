217874cc6b0d5183179b81d6b6c991235441c1d1
Rabbithols <roberturturica@gmail.com>
2019-12-22 12:37:46 +0100

    feat: Ora il form la selezione della lingua si basa sul pipe async di angular

--- src/app/modules/application-form/components/edit/step-lingua/step-lingua.component.ts
+++ src/app/modules/application-form/components/edit/step-lingua/step-lingua.component.ts
@@ -1,11 +1,11 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
-import {MatStepper} from '@angular/material';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import { DomandaService} from '../../../../../core/services/domanda.service';
 import {Lingua} from '../../../../../core/models/rest/rest-interface';
 import {RestService} from '../../../../../core/services/rest.service';
 import {FormService} from '../../../../../core/services/form.service';
 import {FormGroup} from '@angular/forms';
 import {Logger} from '../../../../../core/services';
+import {Observable} from 'rxjs';
 
 
 @Component({
@@ -15,52 +15,28 @@ import {Logger} from '../../../../../core/services';
   styleUrls: ['./step-lingua.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StepLinguaComponent implements OnInit, OnChanges {
+export class StepLinguaComponent implements OnInit {
   @Input() form: FormGroup;
   log: Logger;
-
-  elencoLingue: Lingua[];
-  @ViewChild('stepper', { static: false }) private myStepper: MatStepper;
+  $elencoLingue: Observable<Lingua[]>;
 
 
   constructor(private domandaService: DomandaService,
               private formService: FormService,
-              private detectionChange: ChangeDetectorRef,
-  private rest: RestService) {
+              private rest: RestService) {
     this.log = new Logger('STEP_LINGUA');
-  }
-
-  /* Assegna la lingua al form dal mat button toggle */
-  public onValChange(val: string) {
-    this.formService.linguaSelezionata.patchValue(val);
+    this.$elencoLingue = this.rest.getLingueStraniere();
   }
 
   ngOnInit() {
-    this.rest.getLingueStraniere().subscribe((lingua: Lingua[]) => {
-
-      this.elencoLingue = lingua;
-
       if (this.domandaService.isEditable) {
-
-        const linguaSelezionata = this.elencoLingue
-          .filter((x) => x.id === this.domandaService.domandaobj.domanda.lingua.id)
-          .reduce(z => z);
-        this.formService.linguaSelezionata.patchValue(linguaSelezionata);
+        this.$elencoLingue.subscribe((lingue: Lingua[]) => {
+          const idLinguaSelezionata = this.domandaService.domandaobj.domanda.lingua.id;
+          this.formService.patchFromObject(idLinguaSelezionata, lingue, this.formService.linguaSelezionata, 'id');
+        });
       }
-
-      /*
-       * Siccome il componente è settato su onPush, non ricontrolla da solo i cambiamenti.
-       * Una volta che la lista delle lingue è stata scaricato, avvisiamo angular di ricontrollare la view.
-       * docs: https://angular.io/api/core/ChangeDetectorRef
-      */
-      this.detectionChange.markForCheck();
-    });
   }
 
-
-  ngOnChanges(changes: SimpleChanges): void {
-    this.log.debug(changes);
-  }
 
   getSingleForm(id: string) {
     return this.formService.form.get('formLingua.' + id);
